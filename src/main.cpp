5e884ef64424745391045cf5424a3c61e4432a79
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-12-14 22:48:34 -0500

    Added testing.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,5 @@
-#include "vector.hpp"
-#include <iostream>
-
-using namespace std;
+#include "test/matrix_sum.hpp"
 
 int main () {
-
-    double my_vector[3] = { 3.1, 2.2, 1.3 };
-
-    Vector<3> v1 {my_vector};
-
-    cout << v1 << "\n";
+    doSumTests();
 }
