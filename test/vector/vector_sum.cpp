1544c30459749f45217b9048b02f12d8d0f8f33a
nsdigirolamo <nsdigirolamo@gmail.com>
2023-12-17 18:01:18 -0500

    Added more basic tests for subtraction.

--- test/vector/vector_sum.cpp
+++ test/vector/vector_sum.cpp
@@ -11,8 +11,30 @@ bool testVectorSumBasic () {
     return printTest("testVectorSumBasic", isApprox(v4, v3, EPSILON), v4);
 }
 
+bool testVectorDiffBasic () {
+
+    Vector<3> v1 { 1.0, 2.0, 3.0 };
+    Vector<3> v2 { 9.0, 8.0, 7.0 };
+    Vector<3> v3 { -8.0, -6.0, -4.0};
+    Vector<3> v4 = v1 - v2;
+
+    return printTest("testVectorDiffBasic", isApprox(v4, v3, EPSILON), v4);
+}
+
+bool testVectorDiffReverse () {
+
+    Vector<3> v1 { 1.0, 2.0, 3.0 };
+    Vector<3> v2 { 9.0, 8.0, 7.0 };
+    Vector<3> v3 { 8.0, 6.0, 4.0};
+    Vector<3> v4 = v2 - v1;
+
+    return printTest("testVectorDiffReverse", isApprox(v4, v3, EPSILON), v4);
+}
+
 void doVectorSumTests () {
 
     testVectorSumBasic();
+    testVectorDiffBasic();
+    testVectorDiffReverse();
 
 }
