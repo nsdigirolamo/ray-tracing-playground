bbc075acb5cc2864801543a7524dfcdc0f299627
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-12-16 23:38:25 -0500

    Removed unecessary code.

--- test/vector/vector_sum.cpp
+++ test/vector/vector_sum.cpp
@@ -3,14 +3,9 @@
 
 bool testVectorSumBasic () {
 
-    double my_array[] { 1, 2, 3 };
-
     Vector<3> v1 { 1.0, 2.0, 3.0 };
-
     Vector<3> v2 { 1.0, 2.0, 3.0 };
-
     Vector<3> v3 { 2.0, 4.0, 6.0 };
-
     Vector<3> v4 = v1 + v2;
 
     return printTest("testVectorSumBasic", isApprox(v4, v3, EPSILON), v4);
