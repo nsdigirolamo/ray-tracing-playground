6f3916d00a2e5b38c680ce9d246ae26aef79da67
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-12-16 19:13:31 -0500

    Added testing to make sure matrices are initialized properly with arrays.

--- test/matrix_init.cpp
+++ test/matrix_init.cpp
@@ -1,7 +1,7 @@
 #include "matrix.hpp"
 #include "test/test_utils.hpp"
 
-bool basicInit () {
+bool defaultInit () {
 
     Matrix<3, 3> matrix;
 
@@ -12,11 +12,52 @@ bool basicInit () {
         }
     }
 
-    return printTest("basicInit", true, matrix);
+    return printTest("defaultInit", true, matrix);
+}
+
+bool arrayInit () {
+
+    double my_array[3][3] = {
+        {1.0, 2.0, 3.0},
+        {4.0, 5.0, 6.0},
+        (7.0, 8.0, 9.0)
+    };
+
+    Matrix<3, 3> matrix { my_array };
+
+    for (int r = 0; r < 3; r++) {
+        for (int c = 0; c < 3; c++) {
+            bool is_approx = floatApprox(matrix[r, c], my_array[r][c], EPSILON);
+            if (!is_approx) return printTest("basicInit", false, matrix);
+        }
+    }
+
+    return printTest("arrayInit", true, matrix);
+}
+
+bool arrayInitIsDeep () {
+
+    double my_array[3][3] = {
+        {1.0, 2.0, 3.0},
+        {4.0, 5.0, 6.0},
+        (7.0, 8.0, 9.0)
+    };
+
+    Matrix<3, 3> matrix { my_array };
+
+    my_array[0][0] = 100.0;
+
+    return printTest(
+        "properArrayInit",
+        !floatApprox(matrix[0, 0], my_array[0][0], EPSILON),
+        matrix
+    );
 }
 
 void doMatrixInitTests () {
 
-    basicInit();
+    defaultInit();
+    arrayInit();
+    arrayInitIsDeep();
 
 }
