8fa268d67f7e83af42377500acccbfdfdec4e087
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-12-16 19:25:16 -0500

    Added headers to output.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
+
 #include "test/matrix_init.hpp"
 #include "test/matrix_sum.hpp"
 
+using namespace std;
+
 int main () {
 
+    cout << "--------------------\nMatrix Sum Tests:\n--------------------\n";
     doMatrixSumTests();
+    cout << "--------------------\nMatrix Init Tests:\n--------------------\n";
     doMatrixInitTests();
 
 }
