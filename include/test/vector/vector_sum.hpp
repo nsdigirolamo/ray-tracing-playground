a1e11238b4c28981a32f2aa7979f10f38e1be08d
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-12-16 22:48:54 -0500

    Added vector tests

---
+++ include/test/vector/vector_sum.hpp
@@ -0,0 +1,6 @@
+#ifndef VECTOR_SUM_HPP
+#define VECTOR_SUM_HPP
+
+bool doVectorSumTests ();
+
+#endif
---
+++ test/vector/vector_sum.cpp
@@ -0,0 +1,33 @@
+#include "test/test_utils.hpp"
+#include "vector.hpp"
+
+bool testVectorSumBasic () {
+
+    Vector<3> v1 { { {
+        {1.0},
+        {2.0},
+        {3.0}
+    } } };
+
+    Vector<3> v2 { { {
+        {1.0},
+        {2.0},
+        {3.0}
+    } } };
+
+    Vector<3> v3 { { {
+        {2.0},
+        {4.0},
+        {6.0}
+    } } };
+
+    Vector<3> v4 = { v1 + v2 };
+
+    return printTest("testVectorSumBasi", isApprox(v4, v3, EPSILON), v4);
+}
+
+void doVectorSumTests () {
+
+    testVectorSumBasic();
+
+}
