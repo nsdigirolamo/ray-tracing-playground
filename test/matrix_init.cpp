54b3782f173064c9c65e312f8ef68da5742f52a0
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-12-16 19:43:40 -0500

    Added test for changed a matrix's value.

--- test/matrix_init.cpp
+++ test/matrix_init.cpp
@@ -54,10 +54,30 @@ bool arrayInitIsDeep () {
     );
 }
 
+bool setMatrixValue () {
+
+    double my_array[3][3] = {
+        {1.0, 2.0, 3.0},
+        {4.0, 5.0, 6.0},
+        (7.0, 8.0, 9.0)
+    };
+
+    Matrix<3, 3> matrix { my_array };
+
+    matrix[0, 0] = 100.0;
+
+    return printTest(
+        "setMatrixValue",
+        !floatApprox(matrix[0, 0], my_array[0][0], EPSILON),
+        matrix
+    );
+}
+
 void doMatrixInitTests () {
 
     defaultInit();
     arrayInit();
     arrayInitIsDeep();
+    setMatrixValue();
 
 }
