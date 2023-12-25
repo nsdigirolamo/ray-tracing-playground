03f3d7d2f6fc0b7807c4178c0b9de2fa092b1598
nsdigirolamo <nsdigirolamo@gmail.com>
2023-12-25 17:07:24 -0500

    Now using test framework

--- include/test/test_utils.hpp
+++ include/test/test_utils.hpp
@@ -1,16 +1,17 @@
 #ifndef TEST_UTILS_HPP
 #define TEST_UTILS_HPP
 
+#include "lib/catch2/catch.hpp"
 #include "matrix.hpp"
 #include "test/test_utils.hpp"
 
 #define EPSILON 0.0001
 
 template <unsigned int height, unsigned int width>
-void compare_matrix(Matrix<height, width> m1, Matrix<height, width> m2) {
+void compare_matrix (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
     for (int row = 0; row < height; row++) {
-        for (int col = 0; col < height; col++) {
-            CHECK(m1[row, col] == m2[row, col]);
+        for (int col = 0; col < width; col++) {
+            CHECK(lhs[row, col] == Approx(rhs[row, col]));
         }
     }
 }
