bbd2f3276ea5c24f98bc914e2daae08833a2229c
nsdigirolamo <nsdigirolamo@gmail.com>
2023-12-25 17:13:14 -0500

    Removed unneeded include and define

--- include/test/test_utils.hpp
+++ include/test/test_utils.hpp
@@ -3,9 +3,6 @@
 
 #include "lib/catch2/catch.hpp"
 #include "matrix.hpp"
-#include "test/test_utils.hpp"
-
-#define EPSILON 0.0001
 
 template <unsigned int height, unsigned int width>
 void compare_matrix (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
