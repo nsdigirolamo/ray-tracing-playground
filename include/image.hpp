8cfd56ac706c688148125291bdaa4e702a9b83e3
nsdigirolamo <nsdigirolamo@gmail.com>
2023-12-25 17:20:02 -0500

    Fixed file output

--- include/image.hpp
+++ include/image.hpp
@@ -1,7 +1,7 @@
 #ifndef IMAGE_HPP
 #define IMAGE_HPP
 
-#include <iostream>
+#include <fstream>
 #include "color.hpp"
 
 using namespace std;
@@ -9,13 +9,13 @@ using namespace std;
 template<size_t width, size_t height>
 void generate_ppm (const array<array<Color, width>, height> pixels, const string file_name) {
 
-    ofstream out(file_name + ".ppm");
-    out << "P3\n" << width << " " << height << "\n" << "255\n";
+    ofstream file(file_name + ".ppm");
+    file << "P3\n" << width << " " << height << "\n" << "255\n";
 
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
             Color pixel = pixels[row][col];
-            out << pixel[0] << " " << pixel[1] << " " << pixel[2] << "\n";
+            file << pixel[0] << " " << pixel[1] << " " << pixel[2] << "\n";
         }
     }
 }
