86d12dc413048a5567497bd4a09be08cbda486f8
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-12-28 15:03:57 -0500

    Changed pixels type to fit new function definition

--- src/image.cpp
+++ src/image.cpp
@@ -1,13 +1,13 @@
-#include <array>
+#include <vector>
 #include "color.hpp"
 #include "image.hpp"
 
 void generate_test () {
 
-    array<array<Color, 3>, 2> pixels = {{
-        {{ {{255, 0, 0}}, {{0, 255, 0}}, {{0, 0, 255}} }},
-        {{ {{255, 255, 0}}, {{255, 255, 255}}, {{0, 0, 0}} }}
-    }};
+    Color pixels[2][3] = {
+        { {{255, 0, 0}}, {{0, 255, 0}}, {{0, 0, 255}} },
+        { {{255, 255, 0}}, {{255, 255, 255}}, {{0, 0, 0}} }
+    };
 
     generate_ppm(pixels, "test");
 }
