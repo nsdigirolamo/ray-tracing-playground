d15d659e68dccf3276e58da62b4fb9903877b602
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-12-28 16:18:56 -0500

    Added forward declaration for vector.

--- include/color.hpp
+++ include/color.hpp
@@ -1,7 +1,9 @@
 #ifndef COLOR_HPP
 #define COLOR_HPP
 
-#include "vector.hpp"
+template <unsigned int height>
+requires (0 < height)
+class Vector;
 
 using Color = Vector<3>;
 
--- include/point.hpp
+++ include/point.hpp
@@ -1,7 +1,9 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
-#include "vector.hpp"
+template <unsigned int height>
+requires (0 < height)
+class Vector;
 
 using Point = Vector<3>;
 
