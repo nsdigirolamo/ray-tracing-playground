70118db38803f0cb92b094fb35323c1870412e87
nsdigirolamo <nsdigirolamo@gmail.com>
2024-02-12 14:29:51 -0500

    Added more useful messages.

--- include/test/test_utils.hpp
+++ include/test/test_utils.hpp
@@ -11,7 +11,7 @@ template <unsigned int height, unsigned int width>
 void compare_matrix (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
-            CHECK(lhs[row, col] == doctest::Approx(rhs[row, col]));
+            CHECK_MESSAGE((lhs[row, col] == doctest::Approx(rhs[row, col])), "Matrix CHECK for Row: ", row, " Col: ", col);
         }
     }
 }
@@ -22,13 +22,13 @@ void compare_matrix (const Matrix<height, width>& lhs, const Matrix<height, widt
 
 #define compare_hit(lhs, rhs) \
     compare_matrix(lhs.incoming, rhs.incoming); \
-    CHECK_MESSAGE(lhs.distance == doctest::Approx(rhs.distance), "Hit distance check."); \
+    CHECK_MESSAGE(lhs.distance == doctest::Approx(rhs.distance), "Hit CHECK for distance"); \
     compare_ray(lhs.surface_normal, rhs.surface_normal); \
-    CHECK_MESSAGE(lhs.is_front == rhs.is_front, "Hit is_front check.");
+    CHECK_MESSAGE(lhs.is_front == rhs.is_front, "Hit CHECK for is_front");
 
 #define compare_color(lhs, rhs) \
-    CHECK_MESSAGE(lhs[0] == doctest::Approx(rhs[0]), "Color r check."); \
-    CHECK_MESSAGE(lhs[1] == doctest::Approx(rhs[1]), "Color g check."); \
-    CHECK_MESSAGE(lhs[2] == doctest::Approx(rhs[2]), "Color b check.");
+    CHECK_MESSAGE(lhs[0] == doctest::Approx(rhs[0]), "Color CHECK for R"); \
+    CHECK_MESSAGE(lhs[1] == doctest::Approx(rhs[1]), "Color CHECK for G"); \
+    CHECK_MESSAGE(lhs[2] == doctest::Approx(rhs[2]), "Color CHECK for B");
 
 #endif
