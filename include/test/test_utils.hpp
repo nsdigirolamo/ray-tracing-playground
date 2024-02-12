8e2ed277464ba5a7c17a3966fca34db538b0ad1d
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-02-11 22:21:32 -0500

    Now using new test framework.

--- include/test/test_utils.hpp
+++ include/test/test_utils.hpp
@@ -2,7 +2,7 @@
 #define TEST_UTILS_HPP
 
 #include "hit.hpp"
-#include "lib/catch2/catch.hpp"
+#include "lib/doctest/doctest.hpp"
 #include "primitives/matrix.hpp"
 #include "primitives/color.hpp"
 #include "ray.hpp"
@@ -11,13 +11,24 @@ template <unsigned int height, unsigned int width>
 void compare_matrix (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
-            CHECK(lhs[row, col] == Approx(rhs[row, col]));
+            CHECK(lhs[row, col] == doctest::Approx(rhs[row, col]));
         }
     }
 }
 
-void compare_ray (const Ray& lhs, const Ray& rhs);
-void compare_hit (const Hit& lhs, const Hit& rhs);
-void compare_color (const Color& lhs, const Color& rhs);
+#define compare_ray(lhs, rhs) \
+    compare_matrix(lhs.origin, rhs.origin); \
+    compare_matrix(lhs.direction, rhs.direction);
+
+#define compare_hit(lhs, rhs) \
+    compare_matrix(lhs.incoming, rhs.incoming); \
+    CHECK_MESSAGE(lhs.distance == doctest::Approx(rhs.distance), "Hit distance check."); \
+    compare_ray(lhs.surface_normal, rhs.surface_normal); \
+    CHECK_MESSAGE(lhs.is_front == rhs.is_front, "Hit is_front check.");
+
+#define compare_color(lhs, rhs) \
+    CHECK_MESSAGE(lhs[0] == doctest::Approx(rhs[0]), "Color r check."); \
+    CHECK_MESSAGE(lhs[1] == doctest::Approx(rhs[1]), "Color g check."); \
+    CHECK_MESSAGE(lhs[2] == doctest::Approx(rhs[2]), "Color b check.");
 
 #endif
---
+++ test/intersectables_test.cpp
@@ -0,0 +1,185 @@
+#include <memory>
+
+#include "intersectables/plane.hpp"
+#include "intersectables/sphere.hpp"
+#include "lib/doctest/doctest.hpp"
+#include "materials/diffuse.hpp"
+#include "primitives/color.hpp"
+#include "ray.hpp"
+#include "test/test_utils.hpp"
+
+TEST_SUITE ("Sphere Intersections") {
+    TEST_CASE ("Ray intersects sphere.") {
+
+        Sphere sphere {
+            {{0, 0, 10}},
+            1.0,
+            std::make_unique<Diffuse>(RED)
+        };
+
+        SUBCASE ("Zero intersections.") {
+
+            Ray ray {
+                {{0, 0, 0}},
+                {{0, 0, -1}}
+            };
+
+            std::optional<Hit> actual_hit = sphere.intersects(ray);
+
+            CHECK_FALSE(actual_hit.has_value());
+        }
+
+        SUBCASE ("One intersection.") {
+
+            Ray ray {
+                {{1, 0, 0}},
+                {{0, 0, 1}}
+            };
+
+            std::optional<Hit> actual_hit = sphere.intersects(ray);
+
+            REQUIRE(actual_hit.has_value());
+
+            Ray surface_normal {
+                {{1, 0, 10}},
+                {{1, 0, 0}}
+            };
+
+            Hit expected_hit {
+                ray.direction,
+                10,
+                surface_normal,
+                true
+            };
+
+            compare_hit(expected_hit, actual_hit.value());
+        }
+
+        SUBCASE ("Two intersections.") {
+
+            Ray ray {
+                {{0, 0, 0}},
+                {{0, 0, 1}}
+            };
+
+            std::optional<Hit> actual_hit = sphere.intersects(ray);
+
+            REQUIRE(actual_hit.has_value());
+
+            Ray surface_normal {
+                {{0, 0, 9}},
+                {{0, 0, -1}}
+            };
+
+            Hit expected_hit {
+                ray.direction,
+                9,
+                surface_normal,
+                true
+            };
+
+            compare_hit(expected_hit, actual_hit.value());
+        }
+
+        SUBCASE ("One intersection from within sphere.") {
+
+            Ray ray {
+                {{0, 0, 10}},
+                {{0, 0, 1}}
+            };
+
+            std::optional<Hit> actual_hit = sphere.intersects(ray);
+
+            REQUIRE(actual_hit.has_value());
+
+            Ray surface_normal {
+                {{0, 0, 11}},
+                {{0, 0, -1}}
+            };
+
+            Hit expected_hit {
+                ray.direction,
+                1,
+                surface_normal,
+                false
+            };
+
+            compare_hit(expected_hit, actual_hit.value());
+        }
+    }
+}
+
+TEST_SUITE ("Plane Intersections") {
+    TEST_CASE ("Ray intersects plane.") {
+
+        Plane plane {
+            {{0, 0, 0}},
+            {{0, 1, 0}},
+            std::make_unique<Diffuse>(RED)
+        };
+
+        SUBCASE ("Zero intersections.") {
+
+            Ray ray {
+                {{0, 1, 0}},
+                {{0, 0, 1}}
+            };
+
+            std::optional<Hit> actual_hit = plane.intersects(ray);
+
+            CHECK_FALSE(actual_hit.has_value());
+        }
+
+        SUBCASE ("One intersection.") {
+
+            Ray ray {
+                {{0, 1, 0}},
+                {{0, -1, 0}}
+            };
+
+            std::optional<Hit> actual_hit = plane.intersects(ray);
+
+            REQUIRE(actual_hit.has_value());
+
+            Ray surface_normal {
+                {{0, 0, 0}},
+                {{0, 1, 0}}
+            };
+
+            Hit expected_hit {
+                ray.direction,
+                1,
+                surface_normal,
+                true
+            };
+
+            compare_hit(expected_hit, actual_hit.value());
+        }
+
+        SUBCASE ("One intersection from behind plane.") {
+
+            Ray ray {
+                {{0, -1, 0}},
+                {{0, 1, 0}}
+            };
+
+            std::optional<Hit> actual_hit = plane.intersects(ray);
+
+            REQUIRE(actual_hit.has_value());
+
+            Ray surface_normal {
+                {{0, 0, 0}},
+                {{0, -1, 0}}
+            };
+
+            Hit expected_hit {
+                ray.direction,
+                1,
+                surface_normal,
+                false
+            };
+
+            compare_hit(expected_hit, actual_hit.value());
+        }
+    }
+}
