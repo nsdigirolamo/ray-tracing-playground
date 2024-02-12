c2eb2e4fae7788ad00b0fe9f6c7b1464870f5feb
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-02-11 22:22:20 -0500

    Plane hits now reverse their surface normal if a hit occurs from behind

--- src/plane.cpp
+++ src/plane.cpp
@@ -1,7 +1,5 @@
 #include "intersectables/plane.hpp"
 
-const double minimum_distance = 0.0001;
-
 Plane::Plane (const Point& origin, const UnitVector<3>& normal, std::unique_ptr<Material> material)
     : origin(origin)
     , normal(normal)
@@ -19,15 +17,18 @@ std::optional<Hit> Plane::intersects (const Ray &ray) const {
 
         double distance = dot((Vector<3>)(plane_origin - ray_origin), this->normal) / denominator;
 
-        if (distance < minimum_distance) { return {}; }
+        if (distance < MINIMUM_INTERSECT_DISTANCE) { return {}; }
 
         Point intersection = ray.direction * distance + ray.origin;
-        Ray surface_normal = {intersection, this->normal};
+        bool is_front = dot(ray.direction, this->normal) <= 0;
+
+        Ray surface_normal = is_front ? (Ray){intersection, this->normal} : (Ray){intersection, -1.0 * this->normal};
+
         Hit hit = {
-            ray.direction, 
+            ray.direction,
             distance,
             surface_normal,
-            dot(ray.direction, surface_normal.direction) <= 0
+            is_front
         };
 
         return hit;
