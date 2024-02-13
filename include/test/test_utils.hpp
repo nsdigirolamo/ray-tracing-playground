eb57524e86f8b6ee54afdf5915989b711971d8f9
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-02-13 00:00:33 -0500

    Rewrote all testing functions to be macros instead.

--- include/test/test_utils.hpp
+++ include/test/test_utils.hpp
@@ -7,28 +7,42 @@
 #include "primitives/color.hpp"
 #include "ray.hpp"
 
-template <unsigned int height, unsigned int width>
-void compare_matrix (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
-            CHECK_MESSAGE((lhs[row, col] == doctest::Approx(rhs[row, col])), "Matrix CHECK for Row: ", row, " Col: ", col);
-        }
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcomma-subscript"
+
+#define CHECK_MATRIX(lhs, rhs) \
+    REQUIRE(lhs.getRows() == rhs.getRows()); \
+    REQUIRE(lhs.getCols() == rhs.getCols()); \
+    for (int row = 0; row < lhs.getRows(); ++row) { \
+        for (int col = 0; col < lhs.getCols(); ++col) { \
+            CAPTURE(row); \
+            CAPTURE(col); \
+            CHECK(lhs[row, col] == doctest::Approx(rhs[row, col])); \
+        } \
     }
-}
 
-#define compare_ray(lhs, rhs) \
-    compare_matrix(lhs.origin, rhs.origin); \
-    compare_matrix(lhs.direction, rhs.direction);
+#define CHECK_VECTOR(lhs, rhs) \
+    REQUIRE(lhs.getRows() == rhs.getRows()); \
+    REQUIRE(lhs.getCols() == 1); \
+    REQUIRE(lhs.getCols() == rhs.getCols()); \
+    for (int row = 0; row < lhs.getRows(); ++ row) { \
+        CAPTURE(row); \
+        CHECK(lhs[row] == doctest::Approx(rhs[row])); \
+    }
 
-#define compare_hit(lhs, rhs) \
-    compare_matrix(lhs.incoming, rhs.incoming); \
-    CHECK_MESSAGE(lhs.distance == doctest::Approx(rhs.distance), "Hit CHECK for distance"); \
-    compare_ray(lhs.surface_normal, rhs.surface_normal); \
-    CHECK_MESSAGE(lhs.is_front == rhs.is_front, "Hit CHECK for is_front");
+#define CHECK_RAY(lhs, rhs) \
+    CHECK_VECTOR(lhs.origin, rhs.origin); \
+    CHECK_VECTOR(lhs.direction, rhs.direction);
 
-#define compare_color(lhs, rhs) \
-    CHECK_MESSAGE(lhs[0] == doctest::Approx(rhs[0]), "Color CHECK for R"); \
-    CHECK_MESSAGE(lhs[1] == doctest::Approx(rhs[1]), "Color CHECK for G"); \
-    CHECK_MESSAGE(lhs[2] == doctest::Approx(rhs[2]), "Color CHECK for B");
+#define CHECK_HIT(lhs, rhs) \
+    CHECK_VECTOR(lhs.incoming, rhs.incoming); \
+    CHECK(lhs.distance == doctest::Approx(rhs.distance)); \
+    CHECK_RAY(lhs.surface_normal, rhs.surface_normal); \
+    CHECK(lhs.is_front == rhs.is_front);
+
+#define CHECK_COLOR(lhs, rhs) \
+    CHECK(lhs[0] == doctest::Approx(rhs[0])); \
+    CHECK(lhs[1] == doctest::Approx(rhs[1])); \
+    CHECK(lhs[2] == doctest::Approx(rhs[2]));
 
 #endif
