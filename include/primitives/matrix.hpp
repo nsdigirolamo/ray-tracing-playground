79ff6265bf03c20427dfee1452cfcd159036f53e
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-02-25 23:14:21 -0500

    Rewrote matrix class to be compatible with older C++ standards. Will now be able to compile with nvidia compiler.

--- include/primitives/matrix.hpp
+++ include/primitives/matrix.hpp
@@ -5,259 +5,219 @@
 #include <cmath>
 #include <iostream>
 
-template <unsigned int height, unsigned int width>
-requires (0 < height && 0 < width)
+template <unsigned int row_count>
+class Vector;
+
+template <unsigned int column_count>
+class MatrixRow {
+
+    private:
+
+    double columns[column_count];
+
+    public:
+
+    double& operator[] (const unsigned int col) {
+        assert(col < column_count);
+        return this->columns[col];
+    }
+
+    const double& operator[] (const unsigned int col) const {
+        assert(col < column_count);
+        return this->columns[col];
+    }
+};
+
+template <unsigned int row_count, unsigned int column_count>
 class Matrix {
 
     protected:
 
-    double values[height][width]; /** The values of the matrix. */
-
-    unsigned int rows = height; /** The number of rows in the matrix. */
-    unsigned int columns = width; /** The number of columns in the matrix.*/
+    MatrixRow<column_count> values[row_count];
 
     public:
 
-    /**
-     * @brief Constructs a new Matrix with 0 for all values.
-     */
     Matrix () {
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                values[row][col] = 0.0;
+        for (int row = 0; row < row_count; ++row) {
+            for (int col = 0; col < column_count; ++col) {
+                this->values[row][col] = 0;
             }
         }
     }
 
-    /**
-     * @brief Constructs a new Matrix where the elements of the Matrix are the
-     * same as the given array.
-     *
-     * @param values
-     */
-    Matrix (const double (&values)[height][width]) {
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
+    Matrix (const double (&values)[row_count][column_count]) {
+        for (int row = 0; row < row_count; ++row) {
+            for (int col = 0; col < column_count; ++col) {
                 this->values[row][col] = values[row][col];
             }
         }
     }
 
-    unsigned int getRows () const {
-        return this->rows;
-    }
-
-    unsigned int getCols () const {
-        return this->columns;
-    }
-
-    Matrix<height, width>& operator+= (const Matrix<height, width>& rhs) {
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                values[row][col] += rhs[row, col];
+    Matrix<row_count, column_count>& operator+= (const Matrix<row_count, column_count>& rhs) {
+        for (int row = 0; row < row_count; ++row) {
+            for (int col = 0; col < column_count; ++col) {
+                this->values[row][col] += rhs[row][col];
             }
         }
         return *this;
     }
 
-    Matrix<height, width>& operator-= (const Matrix<height, width>& rhs) {
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                values[row][col] -= rhs[row, col];
+    Matrix<row_count, column_count>& operator-= (const Matrix<row_count, column_count>& rhs) {
+        for (int row = 0; row < row_count; ++row) {
+            for (int col = 0; col < column_count; ++col) {
+                this->values[row][col] -= rhs[row][col];
             }
         }
         return *this;
     }
 
-    Matrix<height, width>& operator*= (const double rhs) {
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                values[row][col] *= rhs;
+    Matrix<row_count, column_count>& operator*= (const double rhs) {
+        for (int row = 0; row < row_count; ++row) {
+            for (int col = 0; col < column_count; ++col) {
+                this->values[row][col] *= rhs;
             }
         }
         return *this;
     }
 
-    Matrix<height, width>& operator/= (const double rhs) {
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                values[row][col] /= rhs;
+    Matrix<row_count, column_count>& operator/= (const double rhs) {
+        for (int row = 0; row < row_count; ++row) {
+            for (int col = 0; col < column_count; ++col) {
+                this->values[row][col] /= rhs;
             }
         }
         return *this;
     }
 
-    /**
-     * @brief Gets the value of the Matrix at the given row and column.
-     *
-     * @param row
-     * @param col
-     * @return double&
-     */
-    double& operator[] (const unsigned int row, const unsigned int col) {
-        assert(row < height);
-        assert(col < width);
-        return values[row][col];
+    MatrixRow<column_count>& operator[] (const unsigned int row) {
+        assert(row < row_count);
+        return this->values[row];
     }
 
-    /**
-     * @brief Gets the value of the Matrix at the given row and column.
-     *
-     * @param row
-     * @param col
-     * @return double&
-     */
-    const double& operator[] (const unsigned int row, const unsigned int col) const {
-        assert(row < height);
-        assert(col < width);
-        return values[row][col];
+    const MatrixRow<column_count>& operator[] (const unsigned int row) const {
+        assert(row < row_count);
+        return this->values[row];
     }
 
-    /**
-     * @brief Performs a matrix transformation on the given vector.
-     *
-     * @param vector A single column Matrix.
-     * @return Matrix<height, 1> The transformed vector.
-     */
-    Matrix<height, 1> transform (const Matrix<width, 1>& vector) const {
+    Vector<row_count> transform (const Vector<row_count>& vector) const {
 
-        Matrix<height, 1> result;
+        Vector<row_count> result;
 
-        for (int col = 0; col < width; col++) {
-            Matrix<height, 1> column;
-            for (int row = 0; row < height; row++) {
-                column[row, 0] = this->values[row][col];
+        for (int col = 0; col < column_count; ++col) {
+            Vector<row_count> temp;
+            for (int row; row < row_count; ++row) {
+                temp[row] = this->values[row][col];
             }
-            result += vector[col, 0] * column;
+            result += vector[col] * temp;
         }
 
         return result;
     }
+
+    unsigned int height () const {
+        return row_count;
+    }
+
+    unsigned int width () const {
+        return column_count;
+    }
 };
 
-template <unsigned int height, unsigned int width>
-Matrix<height, width> operator+ (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
-    Matrix<height, width> result = lhs;
+template <unsigned int row_count, unsigned int column_count>
+Matrix<row_count, column_count> operator+ (const Matrix<row_count, column_count>& lhs, const Matrix<row_count, column_count>& rhs) {
+    Matrix<row_count, column_count> result = lhs;
     result += rhs;
     return result;
 }
 
-template <unsigned int height, unsigned int width>
-Matrix<height, width> operator- (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
-    Matrix<height, width> result = lhs;
+template <unsigned int row_count, unsigned int column_count>
+Matrix<row_count, column_count> operator- (const Matrix<row_count, column_count>& lhs, const Matrix<row_count, column_count>& rhs) {
+    Matrix<row_count, column_count> result = lhs;
     result -= rhs;
     return result;
 }
 
-template <unsigned int height, unsigned int width>
-Matrix<height, width> operator* (const Matrix<height, width>& lhs, const double& rhs) {
-    Matrix<height, width> result = lhs;
+template <unsigned int row_count, unsigned int column_count>
+Matrix<row_count, column_count> operator* (const Matrix<row_count, column_count>& lhs, const double& rhs) {
+    Matrix<row_count, column_count> result = lhs;
     result *= rhs;
     return result;
 }
 
-template <unsigned int height, unsigned int width>
-Matrix<height, width> operator* (const double& lhs, const Matrix<height, width>& rhs) {
-    Matrix<height, width> result = rhs;
+template <unsigned int row_count, unsigned int column_count>
+Matrix<row_count, column_count> operator* (const double& lhs, const Matrix<row_count, column_count>& rhs) {
+    Matrix<row_count, column_count> result = rhs;
     result *= lhs;
     return result;
 }
 
-/**
- * @brief Multiplies the given Matrices in an element-wise fashion.
- *
- * @tparam height The height of the Matrices.
- * @tparam width The width of the Matrices/
- * @param lhs The Matrix on the left hand side of the equation.
- * @param rhs The Matrix on the right hand side of the equation.
- * @return Matrix<height, width>
- */
-template<unsigned int height, unsigned int width>
-Matrix<height, width> hadamard (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
-    Matrix<height, width> result;
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
-            result[row, col] = lhs[row, col] * rhs[row, col];
+template<unsigned int row_count, unsigned int column_count>
+Matrix<row_count, column_count> hadamard (const Matrix<row_count, column_count>& lhs, const Matrix<row_count, column_count>& rhs) {
+    Matrix<row_count, column_count> result;
+    for (int row = 0; row < row_count; row++) {
+        for (int col = 0; col < column_count; col++) {
+            result[row][col] = lhs[row][col] * rhs[row][col];
         }
     }
     return result;
 }
 
-template <unsigned int height, unsigned int width>
-Matrix<height, width> operator/ (const Matrix<height, width>& lhs, const double& rhs) {
-    Matrix<height, width> result = lhs;
+template <unsigned int row_count, unsigned int column_count>
+Matrix<row_count, column_count> operator/ (const Matrix<row_count, column_count>& lhs, const double& rhs) {
+    Matrix<row_count, column_count> result = lhs;
     result /= rhs;
     return result;
 }
 
-template <unsigned int height, unsigned int width>
-bool operator== (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
-            if (lhs[row, col] != rhs[row, col]) return false;
+template <unsigned int row_count, unsigned int column_count>
+bool operator== (const Matrix<row_count, column_count>& lhs, const Matrix<row_count, column_count>& rhs) {
+    for (int row = 0; row < row_count; row++) {
+        for (int col = 0; col < column_count; col++) {
+            if (lhs[row][col] != rhs[row][col]) return false;
         }
     }
     return true;
 }
 
-/**
- * @brief Checks if two Matrices are approximately equal.
- *
- * @tparam height The height of the Matrices.
- * @tparam width The width of the Matrices.
- * @param lhs The Matrix on the left hand side of the equation.
- * @param rhs The Matrix on the right hand side of the equation.
- * @param epsilon The minimum absolute difference between any two elements of the Matrices.
- * @return true if the Matrices are approximately equal.
- * @return false if the Matrices are not approximately equal.
- */
-template <unsigned int height, unsigned int width>
-bool isApprox (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs, const double epsilon = 0.001) {
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
-            if (epsilon < fabs(lhs[row, col] - rhs[row, col])) return false;
+template <unsigned int row_count, unsigned int column_count>
+bool isApprox (const Matrix<row_count, column_count>& lhs, const Matrix<row_count, column_count>& rhs, const double epsilon = 0.001) {
+    for (int row = 0; row < row_count; row++) {
+        for (int col = 0; col < column_count; col++) {
+            if (epsilon < fabs(lhs[row][col] - rhs[row][col])) return false;
         }
     }
     return true;
 }
 
-template <unsigned int height, unsigned int width>
-bool operator!= (const Matrix<height, width>& lhs, const Matrix<height, width>& rhs) {
+template <unsigned int row_count, unsigned int column_count>
+bool operator!= (const Matrix<row_count, column_count>& lhs, const Matrix<row_count, column_count>& rhs) {
     return !(lhs == rhs);
 }
 
-template <unsigned int height, unsigned int width>
-std::ostream& operator<< (std::ostream& output, const Matrix<height, width>& m) {
+template <unsigned int row_count, unsigned int column_count>
+std::ostream& operator<< (std::ostream& output, const Matrix<row_count, column_count>& matrix) {
 
     output << "[ ";
-    for (int col = 0; col < width; col++) {
-        output << m[0, col] << " ";
+    for (int col = 0; col < column_count; col++) {
+        output << matrix[0][col] << " ";
     }
     output << "]";
 
-    for (int row = 1; row < height; row++) {
+    for (int row = 1; row < row_count; row++) {
         output << "\n[ ";
-        for (int col = 0; col < width; col++) {
-            output << m[row, col] << " ";
+        for (int col = 0; col < column_count; col++) {
+            output << matrix[row][col] << " ";
         }
         output << "]";
     }
     return output;
 }
 
-/**
- * @brief Generates an identity Matrix with the same dimensions as the given
- * Matrix
- *
- * @tparam height The height of the Matrix.
- * @param matrix
- * @return Matrix<height, height> An identity Matrix.
- */
-template <unsigned int height>
-Matrix<height, height> identity (const Matrix<height, height>& matrix) {
-    Matrix<height, height> identity_matrix;
-    for (int i = 0; i < height; i++) {
-        identity_matrix[i, i] = 1.0;
+template <unsigned int row_count>
+Matrix<row_count, row_count> identity (const Matrix<row_count, row_count>& matrix) {
+    Matrix<row_count, row_count> identity_matrix;
+    for (int i = 0; i < row_count; i++) {
+        identity_matrix[i][i] = 1.0;
     }
     return identity_matrix;
 }
