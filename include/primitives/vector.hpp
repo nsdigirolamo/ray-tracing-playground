9bed6c5ea3eb529d2d0d2a1ba08b387ef91d0b8f
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-02-25 23:12:45 -0500

    Rewrote vector class. Moved some member functions inside. Unit vectors are now part of vector.hpp

--- include/primitives/vector.hpp
+++ include/primitives/vector.hpp
@@ -5,170 +5,120 @@
 
 #include "primitives/matrix.hpp"
 
-template <unsigned int height>
-requires (0 < height)
-class Vector: public Matrix<height, 1> {
+template <unsigned int row_count>
+class Vector : public Matrix<row_count, 1> {
 
     public:
 
-    /**
-     * @brief Constructs a new Vector with 0 for all values.
-     */
     Vector () {
-        for (int row = 0; row < height; row++) {
-            this->values[row][0] = 0.0;
+        for (int row = 0; row < row_count; ++row) {
+            this->values[row][0] = 0;
         }
     }
 
-    /**
-     * @brief Constructs a new Vector where the elements of the Vector are the
-     * same as the given array.
-     *
-     * @param values
-     */
-    Vector (const double (&values)[height]) {
-        for (int row = 0; row < height; row++) {
+    Vector (const double (&values)[row_count]) {
+        for (int row = 0; row < row_count; ++row) {
             this->values[row][0] = values[row];
         }
     }
 
-    /**
-     * @brief Constructs a new Vector object where the elements of the Vector
-     * are the same as the given Matrix.
-     *
-     * @param m
-     */
-    Vector (const Matrix<height, 1>& m) {
-        for (int row = 0; row < height; row++) {
-            this->values[row][0] = m[row, 0];
+    Vector (const Matrix<row_count, 1>& matrix) {
+        for (int row = 0; row < row_count; ++row) {
+            this->values[row][0] = matrix[row][0];
         }
     }
 
-    /**
-     * @brief Gets the value of the vector at the specified row.
-     *
-     * @param row
-     * @return double&
-     */
     double& operator[] (const unsigned int row) {
-        assert(row < height);
+        assert(row < row_count);
         return this->values[row][0];
     }
 
-    /**
-     * @brief Gets the value of the vector at the specified row.
-     *
-     * @param row
-     * @return const double&
-     */
     const double& operator[] (const unsigned int row) const {
-        assert(row < height);
+        assert(row < row_count);
         return this->values[row][0];
+    }
+
+    double length_squared () const {
+        return dot(*this, *this);
+    }
+
+    double length () const {
+        return sqrtf(this->length_squared());
+    }
+
+    Vector<row_count> direction () const {
+        return *this / this->length();
     }
 };
 
-/**
- * @brief Calculates the dot product between two given vectors.
- *
- * @tparam height The size of the given vectors.
- * @param lhs The vector on the left hand side of the equation.
- * @param rhs The vector on the right hand side of the equation.
- * @return double The dot product.
- */
-template <unsigned int height>
-double dot (const Vector<height>& lhs, const Vector<height>& rhs) {
+template <unsigned int row_count>
+class UnitVector: public Vector<row_count> {
+
+    public:
+
+    UnitVector () {
+        for (int row = 0; row < row_count; ++row) {
+            this->values[row][0] = 0;
+        }
+        this->values[0][0] = 1;
+    }
+
+    UnitVector (const Vector<row_count>& vector) {
+        Vector<row_count> unit_vector = vector / vector.length();
+        for (int row = 0; row < row_count; ++row) {
+            this->values[row][0] = unit_vector[row];
+        }
+    }
+
+    UnitVector (const double (&values)[row_count])
+        : UnitVector((Vector<row_count>)(values))
+    { }
+
+    UnitVector (const Matrix<row_count, 1>& matrix)
+        : UnitVector((Vector<row_count>)(matrix))
+    { }
+};
+
+template <unsigned int row_count>
+UnitVector<row_count> normalize (Vector<row_count> vector) {
+    return (UnitVector<row_count>)(vector);
+}
+
+template <unsigned int row_count>
+double dot (const Vector<row_count>& lhs, const Vector<row_count>& rhs) {
     double dot_product = 0.0;
-    for (int row = 0; row < height; row++) {
+    for (int row = 0; row < row_count; ++row) {
         dot_product += lhs[row] * rhs[row];
     }
     return dot_product;
 }
 
-/**
- * @brief Calculates the cross product between two given 3 dimensional vectors.
- *
- * @param lhs The vector on the left hand side of the equation.
- * @param rhs The vector on the right hand size of the equation.
- */
 #define cross(lhs, rhs) \
-    {{ \
+    (Vector<3>){{ \
         lhs[1] * rhs[2] - lhs[2] * rhs[1], \
         lhs[2] * rhs[0] - lhs[0] * rhs[2], \
         lhs[0] * rhs[1] - lhs[1] * rhs[0], \
     }}
 
-/**
- * @brief Calculates the length - or magnitude - of the given vector.
- *
- * @tparam height The vector's size.
- * @param v
- * @return double The vector's length.
- */
-template <unsigned int height>
-double length (Vector<height> v) {
-    return sqrtf(dot(v, v));
-}
-
-/**
- * @brief Calculates the length - or magnitude - squared of the given vector.
- *
- * @tparam height The vector's size.
- * @param v
- * @return double The vector's length squared.
- */
-template <unsigned int height>
-double length_squared(Vector<height> v) {
-    return dot(v, v);
-}
-
-/**
- * @brief Calculates a unit vector with the same direction of the given vector.
- *
- * @tparam height The vector's size.
- * @param v
- * @return Vector<height> A unit vector.
- */
-template <unsigned int height>
-Vector<height> unit (const Vector<height>& v) {
-    return v / length(v);
-}
-
-/**
- * @brief Reflects a given vector off some given surface.
- *
- * @tparam height The vector's size.
- * @param vector The vector to be reflected.
- * @param surface_normal The surface normal the vector is reflecting off.
- * @return Vector<height> The reflected vector.
- */
-template <unsigned int height>
-Vector<height> reflect (const Vector<height>& vector, const Vector<height>& surface_normal) {
-    Vector<height> normal = unit(surface_normal);
+template <unsigned int row_count>
+Vector<row_count> reflect (const Vector<row_count>& vector, const Vector<row_count>& surface_normal) {
+    Vector<row_count> normal = normalize(surface_normal);
     return vector - 2 * dot(vector, normal) * normal;
 }
 
-/**
- * @brief Refracts a given vector through some material.
- *
- * @tparam height The vector's size.
- * @param vector The vector to be refracted.
- * @param surface_normal The surface normal the vector is refracting through.
- * @param refractive_index The refractive index of the material the vector is refracting through.
- * @return Vector<height> The refracted vector.
- */
-template <unsigned int height>
-Vector<height> refract (const Vector<height>& vector, const Vector<height>& surface_normal, const double refractive_index) {
+template <unsigned int row_count>
+Vector<row_count> refract (const Vector<row_count>& vector, const Vector<row_count>& surface_normal, const double refractive_index) {
 
-    Vector<height> v = unit(vector);
-    Vector<height> n = unit(surface_normal);
+    Vector<row_count> v = normalize(vector);
+    Vector<row_count> n = normalize(surface_normal);
 
     double cos_theta = dot((Vector<3>)(-1.0 * v), n);
     Vector<3> normal_orthogonal = refractive_index * (v + cos_theta * n);
-    Vector<3> normal_parallel = -1.0 * sqrt(fabs(1.0 - length_squared(normal_orthogonal))) * n;
+    Vector<3> normal_parallel = -1.0 * sqrt(fabs(1.0 - normal_orthogonal.length_squared())) * n;
 
     return normal_orthogonal + normal_parallel;
 }
 
-using Point = Vector<3>;
+typedef Vector<3> Point;
 
 #endif
