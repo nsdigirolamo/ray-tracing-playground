d903af573251223c68d29ab5d595e337593aef62
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2024-03-09 20:48:21 -0500

    Lots of changes to fix kernels.

--- bench/bench.cu
+++ bench/bench.cu
@@ -2,12 +2,34 @@
 
 #include <cuda.h>
 #include <iostream>
+#include <cmath>
 
 #include "lib/doctest/doctest.hpp"
 #include "lib/nanobench/nanobench.h"
-#include "primitives/matrix.cuh"
+#include "primitives/matrix.hpp"
 #include "random_utils.hpp"
 #include "test/test_utils.hpp"
+#include "primitives/color.hpp"
+
+void handleCudaError (cudaError_t e, const char* identifier, bool exits = true) {
+
+    fprintf(stdout, "Checking for error in %s... ", identifier);
+
+	if (e == cudaSuccess) {
+        fprintf(stdout, "No Error Found!\n");
+        return;
+    }
+
+	fprintf(stdout, "%s %s: %s\n",
+		identifier,
+		cudaGetErrorName(e),
+		cudaGetErrorString(e)
+	);
+
+	fflush(NULL);
+
+	if (exits) exit(1);
+}
 
 #define HEIGHT 256
 #define WIDTH 256
@@ -60,11 +82,19 @@ TEST_CASE("Compare Addition") {
 
     ankerl::nanobench::Bench b;
 
+    /**
+
     b.minEpochIterations(1000).run("Serial + Operation", [&] {
 
         host_sum = host_lhs + host_rhs;
 
     });
+
+    */
+    
+    host_sum = host_lhs + host_rhs;
+
+    /**
 
     b.minEpochIterations(1000).run("Parallel + Operation", [&] {
 
@@ -73,7 +103,149 @@ TEST_CASE("Compare Addition") {
 
     });
 
+    */
+
+    add<<<grid_dims, block_dims>>>(device_sum, device_lhs, device_rhs);
+    cudaDeviceSynchronize();
+
     cudaMemcpy(&empty_host_sum, device_sum, SIZE, cudaMemcpyDeviceToHost);
 
     CHECK_MATRIX(host_sum, empty_host_sum);
 }
+
+#define PIXEL_HEIGHT 4
+#define PIXEL_WIDTH 4
+#define PIXEL_DEPTH 4
+#define MULTIPLIER 2
+
+__global__ void multiply (Color* pixels, double multiplier) {
+
+    int row = blockIdx.y;
+    int col = blockIdx.x;
+    int depth = threadIdx.x;
+
+    pixels[(depth * PIXEL_HEIGHT * PIXEL_WIDTH) + (row * PIXEL_WIDTH) + col] *= 2;
+}
+
+TEST_CASE("Testing Unified (Managed) Memory") {
+
+    Color* pixels;
+    cudaError_t e;
+
+    const int height = PIXEL_HEIGHT;
+    const int width = PIXEL_WIDTH;
+    const int depth = PIXEL_DEPTH;
+    const double multiplier = MULTIPLIER;
+
+    size_t size = sizeof(Color) * height * width * depth;
+
+    e = cudaMallocManaged(&pixels, size);
+    handleCudaError(e, "Managed Malloc");
+
+    Color* expected_result = (Color*)(malloc(size));
+
+    for (int row = 0; row < height; ++row) {
+        for (int col = 0; col < width; ++col) {
+            for (int level = 0; level < depth; ++level) {
+
+                Color val { fabs(randomDouble()), fabs(randomDouble()), fabs(randomDouble()) };
+                pixels[(level * height * width) + (row * width) + col] = val;
+                std::cout << pixels[(level * height * width) + (row * width) + col] << "\n\n";
+                expected_result[(level * height * width) + (row * width) + col] = val * multiplier;
+
+            }
+        }
+    }
+
+    ankerl::nanobench::Bench b;
+
+    /**
+
+    b.epochIterations(1).run("Pixel 3D Array Multiply", [&] {
+
+        dim3 grid_dims(height, width);
+        dim3 block_dims(depth);
+
+        multiply<<<grid_dims, block_dims>>>(pixels, multiplier);
+
+        e = cudaDeviceSynchronize();
+        handleCudaError(e, "Synchronize");
+    });
+
+    */
+
+    dim3 grid_dims(height, width);
+    dim3 block_dims(depth);
+
+    multiply<<<grid_dims, block_dims>>>(pixels, multiplier);
+
+    e = cudaDeviceSynchronize();
+    handleCudaError(e, "Synchronize");
+
+    for (int row = 0; row < height; ++row) {
+        for (int col = 0; col < width; ++col) {
+            for (int level = 0; level < depth; ++level) {
+                Color actual_pixel = pixels[(level * height * width) + (row * width) + col];
+                Color expected_pixel = expected_result[(level * height * width) + (row * width) + col];
+                std::cout << actual_pixel << "\n\n";
+                CHECK_COLOR(actual_pixel, expected_pixel);
+            }
+        }
+    }
+}
+
+/**
+__global__ void pitched_array_test (cudaPitchedPtr pitched_ptr) {
+
+    double* elements = (double*)(pitched_ptr.ptr);
+    size_t pitch = pitched_ptr.pitch;
+    int width = pitched_ptr.xsize;
+    int height = pitched_ptr.ysize;
+
+    int row = blockIdx.y;
+    int col = blockIdx.x;
+    int depth = threadIdx.x;
+
+    double* element = elements + ((depth * height * pitch) + (row * pitch) + col);
+
+    *element *= 2;
+}
+ *
+
+TEST_CASE("Testing cudaPitchPtr") {
+
+    const int width = 8;
+    const int height = 8;
+    const int depth = 8;
+
+    double* host_nums = (double*) malloc(sizeof(double) * width * height * depth);
+    double* host_result = (double*) malloc(sizeof(double) * width * height * depth);
+    double* extra_host_result = (double*) malloc(sizeof(double) * width * height * depth);
+
+    for (int row = 0; row < height; ++row) {
+        for (int col = 0; col < width; ++col) {
+            for (int level = 0; level < depth; ++level) {
+                int i = level + row + col;
+                host_nums[(level * height * width) + (row * width) + col] = i;
+                host_result[(level * height * width) + (row * width) + col] = i * 2;
+            }
+        }
+    }
+
+    cudaArray *pixels;
+
+    cudaMalloc3DArray(&pixels, );
+
+
+    //cudaExtent pixelExtent = make_cudaExtent(sizeof(Color) * width, height, depth);
+
+    //cudaMalloc3D(&pixels, pixelExtent);
+
+    cudaMemcpy3DParms params;
+
+    params.srcPtr = make_cudaPitchedPtr(pixels, sizeof(Color) * width, width, height);
+
+
+}
+
+*/
